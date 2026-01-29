903a4eccbbfe507d1639fd38306f11c101d47930
asztalosm <23d_asztalosm@nyirszikszi.hu>
2026-01-29 22:46:18 +0100

    questions stored in files, some code added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
@@ -6,7 +7,6 @@ public class Main {
     public static Scanner scn = new Scanner(System.in);
     public static final String TF_RESET = "\u001B[0m";
     public static final String TF_BOLD = "\u001B[1m";
-    public static final String TF_UNDERLINE = "\u001B[1m";
     public static final String TF_GREEN = "\u001B[32m";
     public static final String TF_BLUE = "\u001B[34m";
     public static final String TF_RED = "\u001B[31m";
@@ -14,81 +14,40 @@ public class Main {
     public static ArrayList<Map<String, String>> easyQuestions = new ArrayList<>();
     public static ArrayList<Map<String, String>> mediumQuestions = new ArrayList<>();
     public static ArrayList<Map<String, String>> hardQuestions = new ArrayList<>();
-    public static Map<String, String> map = new HashMap<>();
-    public static void exit() {
-        scn.close();
-        System.exit(0);
+    public static ArrayList<Map<String, String>> gameQuestions = new ArrayList<>();
+
+
+    public static void setQuestionsFromTxts() {
+        //feltölt egy arraylistet kérdésekkel és válaszokkal, arraylistből majd törölni fogunk
+        File easyQuestionsFile = new File("src/easyQuestions");
+        File mediumQuestionsFile = new File("src/mediumQuestions");
+        File hardQuestionsFile = new File("src/hardQuestions");
+        try (Scanner fileReader = new Scanner(easyQuestionsFile)) {
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
+                String[] questionData = data.split("><"); //we split like this so if we were to write a code snippet the data won't get fucked
+                easyQuestions.add(new HashMap<>() {{
+                    put("Question", questionData[0]);
+                    put("Answer1", questionData[1]);
+                    put("Answer2", questionData[2]);
+                    put("Answer3", questionData[3]);
+                    put("Answer4", questionData[4]);
+                    put("Correct", questionData[5]);
+                }});
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Nem található a kérdéseket tartalmazó fájl");
+            //get data from the internet or maybe a failsafe questionnaire embedded in the code
+        }
     }
 
-    public static void setQuestionDictionary() {
-        //Easy Questions
-        easyQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        easyQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
 
-        //Medium Questions
-        mediumQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        mediumQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        mediumQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
 
-        //Hard Questions
-        hardQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        hardQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        hardQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
-        hardQuestions.add(new HashMap<String, String>() {{
-            put("Question", "Question?");
-            put("Answer", "Correct Answer");
-            put("Incorrect1", "incorrect1");
-            put("Incorrect2", "incorrect2");
-            put("Incorrect3", "incorrect3");
-        }});
+    public static void fillGameQuestions() {
+        //todo make a loop for every question
+        for (int i = 0; i < 5; i++) { //easy questions
+
+        }
     }
 
     public static void playgame() {
@@ -96,21 +55,25 @@ public class Main {
         System.out.printf("Könnyű kérdések száma: %d%n", easyQuestions.size());
         System.out.printf("Közepes kérdések száma: %d%n", mediumQuestions.size());
         System.out.printf("Nehéz kérdések száma: %d%n", hardQuestions.size());
+        fillGameQuestions();
+
     }
 
     public static void menu() {
         System.out.printf("%s\tLegyen ön is Milliomos v0.1%n%s", (TF_BOLD + TF_BLUE), (TF_RESET));
         System.out.println("[1] Játék kezdése");
         System.out.println("[2] Kilépés"); //maybe beállítások
-        setQuestionDictionary();
-        String menuUserOption = null;
+        setQuestionsFromTxts();
+        //setQuestionDictionary();
+        String menuUserOption;
         do {
             menuUserOption = String.valueOf(scn.nextLine().charAt(0));
         } while (!menuUserOption.equals("1") && !menuUserOption.equals("2"));
         if (menuUserOption.equals("1")) {
             playgame();
         } else {
-            exit();
+            scn.close();
+            System.exit(0);
         }
     }
 
